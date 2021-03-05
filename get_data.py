6a53e2fe2a2b898b86c473973cb88870e4ee90ef
Ben Elsworth <be15516@bristol.ac.uk>
2021-03-05 23:52:59 +0000

    person page search

--- get_data.py
+++ get_data.py
@@ -7,6 +7,9 @@ def read_emails():
     df = pd.read_csv('data/email.txt',names=['email'])
     print(df.head())
 
+    # check for dups
+    df.drop_duplicates(inplace=True)
+
     pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@bristol.ac.uk$)") 
     email_check = df['email'].apply(lambda x: True if pattern.match(x) else False)
     bad_emails = df[~email_check]
@@ -23,14 +26,29 @@ def uob_finder_web(email):
     url=f"https://research-information.bris.ac.uk/en/searchAll/advanced/?exactPhrase={email}&or=&minus=&family=persons&doSearch=Search&slowScroll=true&resultFamilyTabToSelect="
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    find_person = soup.find_all("a", class_="link person")
-    print(len(find_person))
-    if len(find_person)==1:
-        print(find_person)
+    people_data = soup.find_all("li", class_="grid-result-item")
+    person_page = False
+    for p in people_data:
+        find_person = p.find_all("a", class_="link person")
+        #print(p)
+        e = p.find("span", text=re.compile(email, re.IGNORECASE))
+        if e:
+            #print(find_person,e)
+            person_page = find_person[0]['href']
+            print(person_page)
+    if person_page == False:
+        print('No page found')
+        return 'NA'
     else:
-        print(f'Error, more than one person {len(find_person)} \n{find_person}')
+        return person_page
 
-email_df = read_emails()
-for i,row in email_df.head().iterrows():
-    uob_finder_web(email=row['email'])
-
+data_df = read_emails()
+person_pages = []
+for i,row in data_df.iterrows():
+    person_page = uob_finder_web(email=row['email'])
+    person_pages.append(person_page)
+data_df['person_page']=person_pages
+data_df.to_csv('data/person_pages.tsv',sep='\t',index=False)
+#person_page = uob_finder_web(email='D.Wilson@bristol.ac.uk')
+#print(person_page)
+#uob_finder_web(email='ben.elsworth@bristol.ac.uk')
